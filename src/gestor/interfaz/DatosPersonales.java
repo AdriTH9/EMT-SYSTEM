7047dbf158c39b8196983131aac5e23e72eae295
AdriTH9 <tlaxcaltecatladriana40@gmail.com>
2024-04-04 11:37:24 -0600

    2da interfaz, 1ra parte

--- src/gestor/interfaz/DatosPersonales.java
+++ src/gestor/interfaz/DatosPersonales.java
@@ -4,5 +4,16 @@ import javax.swing.*;
 
 public class DatosPersonales {
     private JPanel panel1;
-    private JTextField textField1;
+    private JTextField textFielId;
+    private JTextField textFieldCorreoElec;
+    private JButton btnAgregar;
+    private JLabel id;
+    private JLabel nomcompleto;
+    private JTextField textFielNomCompleto;
+    private JLabel correoelectronico;
+    private JButton btnCerrar;
+
+    public DatosPersonales(){
+
+    }
 }
